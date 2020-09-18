9730a054d4e90a54ed66934963feec144ef1eedb
QZ-98 <60328123+QAZI-98@users.noreply.github.com>
2020-09-19 02:52:13 +0500

    initial

---
+++ app/src/main/java/com/example/doctorapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.doctorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "hellow world", Toast.LENGTH_SHORT).show();
+    }
+}
